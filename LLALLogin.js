312e0106d5046f7d85cefa13a747a301ebeb5ca3
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2022-03-02 15:13:59 -0500

    Update LLALLogin.js
    made a regex checker function which shortens a lot of the code.

--- LLALLogin.js
+++ LLALLogin.js
@@ -14,7 +14,19 @@ for (let i = 0; i < requiredFields.length; i++){
     requiredFields[i].innerHTML =  requiredFields[i].innerHTML + " *"
 }
 
+function login_regex_test(element, regex, alert_message){
+    element_value = element.value.trim();
+
+    if (regex.test(element_value) == false){
+        alert(alert_message)
+        element.focus();
+    }
+    
+    return regex.test(element_value);
+}
+
 function validation(){
+    const re_empty_string= /^(?!\s*$).+/;
     const re_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const re_password_length = /^.{0,12}$/;
     const re_uppercase = /[A-Z]/;
@@ -23,97 +35,30 @@ function validation(){
     const re_id = /[0-9]*6/;
     const re_phone = /^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
 
-    let firstname = document.getElementById("first").value.trim();
-    let lastname = document.getElementById("last").value.trim();
-    let email = document.getElementById("email").value.trim();
-    let password = document.getElementById("password").value.trim();
-    let id = document.getElementById("id").value.trim();
-    let phonenumber = document.getElementById("phone-number").value.trim();
+    let firstname = document.getElementById("first");
+    let lastname = document.getElementById("last");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let id = document.getElementById("id");
+    let phonenumber = document.getElementById("phone-number");
     let email_required = document.getElementById("email-confirmation").checked;
-    let transaction = document.getElementById("transaction").value.trim();
+    let transaction = document.getElementById("transaction");
 
-    if(firstname == ""){
-        alert("First Name is Required!");
-        document.getElementById("first").focus();
-        return;
-    }
+    if(login_regex_test(firstname,re_empty_string,"First Name is Required!") == false){return}
+    if(login_regex_test(lastname,re_empty_string,"Last Name is Required!") == false){return}
+    if(email_required && login_regex_test(email,re_empty_string,"Email is Required!") == false){return}
+    if(email_required && login_regex_test(email,re_email,"Email is Invalid!") == false){return}
+    if(login_regex_test(password,re_empty_string,"Password is Required!") == false){return}
+    if(login_regex_test(password,re_password_length,"Password Must Not Exceed 12 Characters!") == false){return}
+    if(login_regex_test(password,re_uppercase,"Password Must Contain an Uppercase Letter!") == false){return}
+    if(login_regex_test(password,re_special,"Password Must Contain a Special Character!") == false){return}
+    if(login_regex_test(password,re_numeric,"Password Must Contain a Number!") == false){return}
+    if(login_regex_test(firstname,re_empty_string,"ID is Required!") == false){return}
+    if(login_regex_test(id,re_id,"ID Must Be 6 Digits Long!") == false){return}
+    if(login_regex_test(phonenumber,re_empty_string,"Phone Number is Required!") == false){return}
+    if(login_regex_test(phonenumber,re_phone,"Phone Number is Wrong Format!") == false){return}
 
-    if(lastname == ""){
-        alert("Last Name is Required!");
-        document.getElementById("last").focus();
-        return;  
-    }
-
-    if (email_required && email == ""){
-        alert("Email is Required!");
-        document.getElementById("email").focus();
-        return;
-    }
-
-    if(email_required){
-        if(re_email.test(email) == false){
-            alert("Email is Invalid!")
-            return;
-        }
-    }
-
-    if (password == ""){
-        alert("Password is Required!");
-        document.getElementById("password").focus();
-        return;
-    }
-
-    if(re_password_length.test(password) == false){
-        alert("Password Must Not Exceed 12 Characters");
-        document.getElementById("password").focus();
-        return;
-    }
-
-    if (re_uppercase.test(password) == false){
-        alert("Password Must Contain an Uppercase Letter");
-        document.getElementById("password").focus();
-        return;
-    }
-
-    if (re_special.test(password) == false){
-        alert("Password Must Contain a Special Character");
-        document.getElementById("password").focus();
-        return;
-    }
-
-    if (re_numeric.test(password) == false){
-        alert("Password Must Contain a Number");
-        document.getElementById("password").focus();
-        return;
-    }
-
-    if (id == ""){
-        alert("ID is Required!");
-        document.getElementById("id").focus();
-        return
-    }
-
-    if(re_id.test(id) == false){
-        alert("ID Must Be 6 Digits Long");
-        document.getElementById("id").focus();
-        return;
-    }
-
-    if (phonenumber==""){
-        alert("Phone Number is Required!");
-        document.getElementById("phone-number").focus();
-        return;
-    }
-    
-    if(re_phone.test(phonenumber) == false){
-        alert("Phone Number is Wrong Format!");
-        document.getElementById("phone-number").focus();
-        return;
-    }
-
-
-    console.log()
-    verification(firstname.toLowerCase() +" " +lastname.toLowerCase(),password,id,transaction)
+    verification(firstname.value.trim().toLowerCase() +" " +lastname.value.trim().toLowerCase(),password.value.trim(),id.value.trim(),transaction.value.trim())
 }
 
 function verification(name, password, id,transaction){
